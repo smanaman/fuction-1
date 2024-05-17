a6aedf040831dac65385c65d8f39510d36154309
smanaman <sanvatsman@gmail.com>
2024-05-17 01:44:20 -0700

    codec

---
+++ q6.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void hello(){
+int first_number=0;
+int second_number=1;
+int next;
+int n;
+
+
+for(int x=1; x<=10;x++){
+next=first_number+second_number;
+first_number=second_number;
+second_number=next;
+printf("%d\n",next);
+
+}
+
+ 
+  
+}
+int main(){
+
+hello();
+
+}
